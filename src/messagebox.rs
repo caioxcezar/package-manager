dc1b1bb753a85666cc56e1785d1474466826cef1
Caio Rezende <caiocezar50@outlook.com>
2022-06-25 20:27:04 -0300

    password dialog closes on enter. command window auto scroll.

--- src/messagebox.rs
+++ src/messagebox.rs
@@ -35,21 +35,29 @@ pub async fn ask_password(window: Option<gtk::Window>) -> Option<SecVec<u8>> {
         .margin_start(10)
         .build();
     let text = gtk::Label::builder().label("Password").build();
-    let password = gtk::Text::builder().text("").visibility(false).build();
+    let password = gtk::Entry::builder().text("").visibility(false).build();
     let button = gtk::Button::builder().label("Ok").build();
     child.append(&text);
     child.append(&password);
     child.append(&button);
     dialog.set_child(Some(&child));
-    let dialog_clone = dialog.clone();
+
+    let dialog_button = dialog.clone();
+    let dialog_password = dialog.clone();
+
     button.connect_clicked(move |_| {
-        dialog.response(ResponseType::Ok);
+        dialog_button.response(ResponseType::Ok);
     });
-    let response = dialog_clone.run_future().await;
+
+    password.connect_activate(move |_| {
+        dialog_password.response(ResponseType::Ok);
+    });
+
+    let response = dialog.run_future().await;
     if response == ResponseType::Ok {
         let pass = password.text().to_string();
         let check_password = command::run(&format!("echo '{}' | sudo -S su", &pass));
-        dialog_clone.close();
+        dialog.close();
         let res = match check_password {
             Ok(_) => Some(SecStr::from(pass)),
             _ => {
