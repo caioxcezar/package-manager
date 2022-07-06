d1202fe67851ada62d96e1bd0b01e7fc89575e02
Caio Rezende <caiocezar50@outlook.com>
2022-07-06 16:54:48 -0300

    Finished protonge

--- src/backend/api.rs
+++ src/backend/api.rs
@@ -1,10 +1,12 @@
+use flate2::bufread::GzDecoder;
 use gtk::glib;
 use gtk::traits::TextBufferExt;
 use serde::de::DeserializeOwned;
 use std::{
-    io::Cursor,
+    io::BufReader,
     thread::{self, JoinHandle},
 };
+use tar::Archive;
 
 pub fn get<T: DeserializeOwned>(url: &str) -> Result<T, String> {
     let client = reqwest::blocking::Client::new();
@@ -31,11 +33,41 @@ pub fn get<T: DeserializeOwned>(url: &str) -> Result<T, String> {
     Ok(resp)
 }
 
-pub fn download(url: String, file_path: String, text_buffer: &gtk::TextBuffer) -> JoinHandle<bool> {
+pub fn get_str(url: &str) -> Result<String, String> {
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .get(url)
+        .header(reqwest::header::ACCEPT, "*/*")
+        .header(reqwest::header::USER_AGENT, "PackageManager/1.0.0")
+        .send();
+
+    let resp = match resp {
+        Ok(value) => value,
+        Err(value) => {
+            return Err(value.to_string());
+        }
+    };
+
+    let resp = match resp.text() {
+        Ok(value) => value,
+        Err(value) => {
+            return Err(value.to_string());
+        }
+    };
+
+    Ok(resp)
+}
+
+pub fn download_and_extract(
+    url: String,
+    file_path: String,
+    text_buffer: &gtk::TextBuffer,
+) -> JoinHandle<bool> {
     let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
     let txt_buffer = text_buffer.clone();
 
     let tr = thread::spawn(move || {
+        let _ = tx.send("Starting request...\n".to_owned());
         let response = reqwest::blocking::get(url);
         let response = match response {
             Ok(value) => value,
@@ -44,31 +76,14 @@ pub fn download(url: String, file_path: String, text_buffer: &gtk::TextBuffer) -
                 return false;
             }
         };
-        let file = std::fs::File::create(file_path);
-        let mut file = match file {
-            Ok(value) => value,
-            Err(value) => {
-                let _ = tx.send(value.to_string());
-                return false;
-            }
-        };
-        let bytes = response.bytes();
-        let bytes = match bytes {
-            Ok(value) => value,
-            Err(value) => {
-                let _ = tx.send(value.to_string());
-                return false;
-            }
-        };
-        let mut content = Cursor::new(bytes);
-        let copy = std::io::copy(&mut content, &mut file);
-        match copy {
-            Ok(_) => true,
-            Err(value) => {
-                let _ = tx.send(value.to_string());
-                false
-            }
-        }
+        let _ = tx.send("Start downloading...\n".to_owned());
+        let content_br = BufReader::new(response);
+        let tarfile = GzDecoder::new(content_br);
+        let mut archive = Archive::new(tarfile);
+        let _ = tx.send("Downloading and unpacking...\n".to_owned());
+        archive.unpack(file_path).unwrap();
+        let _ = tx.send("Finished. \n".to_owned());
+        true
     });
 
     rx.attach(None, move |text: String| {
